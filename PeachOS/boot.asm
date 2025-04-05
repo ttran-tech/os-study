2105d8b6cedf9b07f89625a4a27c426bf53baddf
Tan <duytantran2710@gmail.com>
2025-04-04 22:08:18 -0700

    Feat: add print function

--- PeachOS/boot.asm
+++ PeachOS/boot.asm
@@ -28,6 +28,7 @@ registers_reset_step2:
 ; =================== PROTECTED MODE SETTING ========================================
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
+VGA_MEMORY equ 0xB8000
 
 load_protected:
     cli ; disable interrupt
@@ -84,14 +85,52 @@ load32:
     mov ebp, 0x00200000
     mov esp, ebp
 
-.print_char:
-    mov esi, 0xB8000    ; VGA text mode
-    mov byte[esi], 'H'
-    mov byte[esi+1], 0x2F ; color code fg: white, bg: green
+start_protected_mode:
+	jmp is_A20_on? ; test for A20 line when starting Protected Mode
+
+enable_A20:	; enable A20 if A20 line is cleared
+	in al, 0x92
+	or al, 2
+	out 0x92, al
+	jmp is_A20_on? ; re-test A20 line
+
+is_A20_on?:
+	pushad
+	mov edi,0x112345  ;odd megabyte address.
+	mov esi,0x012345  ;even megabyte address.
+	mov [esi],esi     ;making sure that both addresses contain diffrent values.
+	mov [edi],edi     ;(if A20 line is cleared the two pointers would point to the address 0x012345 that would contain 0x112345 (edi)) 
+	cmpsd             ;compare addresses to see if the're equivalent.
+	popad
+	jne A20_on        ;if not equivalent , A20 line is set.
+	jmp enable_A20    ;if equivalent, the A20 line is cleared, jmp to enable_A20.
+
+A20_on:
+    mov esi, msg_A20_on             ; prepare the string to call print_string function
+    mov ah, [msg_A20_on_color]    ; add color code
+    call print_string
+    hang:
+        ; infinity loop
+        jmp $
+
+print_string:
+    mov edi, VGA_MEMORY
+    .print_char:
+        lodsb
+        cmp al, 0
+        je .done
+        mov [edi], al
+        mov byte [edi+1], ah
+        add edi, 2
+        jmp .print_char
+    .done:
+        ret
 
 
-    ; infinity loop
-    jmp $
+msg_A20_on: db "A20 is on ", 0
+msg_A20_on_color: db 0x2F
+msg_A20_off: db "A20 is off ", 0
+msg_A20_off_color: db 0xCF
 
 ; Filling the remain bytes to 0
 times 510 - ($-$$) db 0
