6351cfd8ca5fcb3c9501869735760668ecf067b2
Tan <duy@ttran.tech>
2025-04-11 21:37:54 -0700

    Feat: add VGA dimensions

--- PeachOS/src/kernel.h
+++ PeachOS/src/kernel.h
@@ -1,6 +1,10 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+// Define VGA terminal dimensions
+#define VGA_WIDTH 80    // 80 characters wide
+#define VGA_HEIGHT 20   // 20 lines tall
+
 void kernel_main();
 
 #endif
